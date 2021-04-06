583754645cee2ce6cd73e36f0ad289be8ac9333a
janaSunrise <warriordefenderz@gmail.com>
2021-04-07 00:41:46 +0530

    Progress with the utility methods.

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ SPOTIFY_SECRET_ID = os.getenv("SPOTIFY_SECRET_ID")
 # Spotify URL config
 SPOTIFY__GENERATE_TOKEN = "https://accounts.spotify.com/api/token"
 SPOTIFY__REFRESH_TOKEN = "https://accounts.spotify.com/api/token"
+REDIRECT_URI = BASE_URL + "/callback"
 
 SPOTIFY__NOW_PLAYING = "https://api.spotify.com/v1/me/player/currently-playing?additional_types=track,episode"
 SPOTIFY__RECENTLY_PLAYED = "https://api.spotify.com/v1/me/player/recently-played?limit=10"
--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,11 @@ import base64
 
 import requests
 
-from .config import SPOTIFY__REFRESH_TOKEN, SPOTIFY_CLIENT_ID, SPOTIFY_SECRET_ID
+from .config import (
+    SPOTIFY__REFRESH_TOKEN, SPOTIFY__GENERATE_TOKEN,
+    SPOTIFY__NOW_PLAYING, SPOTIFY_CLIENT_ID,
+    SPOTIFY_SECRET_ID, REDIRECT_URI
+)
 
 
 def get_refresh_token(refresh_token):
@@ -15,6 +19,28 @@ def get_refresh_token(refresh_token):
     }
 
     response = requests.post(SPOTIFY__REFRESH_TOKEN, data=payload, headers=headers)
+    return response.json()
+
+
+def generate_token(authorization_code):
+    headers = {"Authorization": f"Basic {generate_base64_auth(SPOTIFY_CLIENT_ID, SPOTIFY_SECRET_ID)}"}
+    payload = {
+        "grant_type": "authorization_code",
+        "code": authorization_code,
+        "redirect_uri": REDIRECT_URI,
+    }
+
+    response = requests.post(SPOTIFY__GENERATE_TOKEN, data=payload, headers=headers)
+    return response.json()
+
+
+def get_now_playing(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(SPOTIFY__NOW_PLAYING, headers=headers)
+
+    if response.status_code != 200:
+        return {}
+
     return response.json()
 
 
