0f70b27dd275bfc69fed2b7fff7c8d08c6704433
janaSunrise <warriordefenderz@gmail.com>
2021-04-07 11:28:48 +0530

    Added utility methods.

--- app/utils.py
+++ app/utils.py
@@ -4,8 +4,10 @@ import requests
 
 from .config import (
     SPOTIFY__REFRESH_TOKEN, SPOTIFY__GENERATE_TOKEN,
-    SPOTIFY__NOW_PLAYING, SPOTIFY_CLIENT_ID,
-    SPOTIFY_SECRET_ID, REDIRECT_URI
+    SPOTIFY__NOW_PLAYING, SPOTIFY__RECENTLY_PLAYED,
+    SPOTIFY__USER_INFO,
+    SPOTIFY_CLIENT_ID, SPOTIFY_SECRET_ID,
+    REDIRECT_URI
 )
 
 
@@ -34,15 +36,31 @@ def generate_token(authorization_code):
     return response.json()
 
 
+def generate_base64_auth(client_id, client_secret):
+    return base64.b64encode(f"{client_id}:{client_secret}".encode()).decode("utf-8")
+
+
 def get_now_playing(access_token):
     headers = {"Authorization": f"Bearer {access_token}"}
     response = requests.get(SPOTIFY__NOW_PLAYING, headers=headers)
 
-    if response.status_code != 200:
+    if response.status_code == 204:
         return {}
 
     return response.json()
 
 
-def generate_base64_auth(client_id, client_secret):
-    return base64.b64encode(f"{client_id}:{client_secret}".encode()).decode("utf-8")
+def get_recently_played(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(SPOTIFY__RECENTLY_PLAYED, headers=headers)
+
+    if response.status_code == 204:
+        return {}
+
+    return response.json()
+
+
+def get_user_info(access_token):
+    headers = {"Authorization": f"Bearer {access_token}"}
+    response = requests.get(SPOTIFY__USER_INFO, headers=headers)
+    return response.json()
